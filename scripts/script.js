538c3dc93f37332da00c9c17b0d51bb5cb9a8713
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2023-12-23 14:52:46 +0100

    Added some changes in order for the script to work, but it does not

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-// TODO: Start and finish script
+// TODO: Function doesn't work. Need to fix it.
+
+function aca() {
+    let result = "";
+    for (let i = 0; i < document.getElementsByClassName('numbers').length; i++) {
+        let number = document.getElementsByClassName('numbers')[i].textContent;
+        result += "Hola" + number;
+    }
+    document.getElementById('aca').textContent = result;
+}
+
+aca();
