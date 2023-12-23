ea74c1b117dfbfe03dc0f5d220e55b67df978e22
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2023-12-23 21:47:14 +0100

    Finished, but I cheated

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,20 @@
-// TODO: Function doesn't work. Need to fix it.
+// (23/dec/2023) Finished, but I cheated because I don't have the skill level yet. I need to keep learning, and when I do, I'll come back to create a new calculator with my own knowledge.
 
-function aca() {
-    let result = "";
-    for (let i = 0; i < document.getElementsByClassName('numbers').length; i++) {
-        let number = document.getElementsByClassName('numbers')[i].textContent;
-        result += "Hola" + number;
-    }
-    document.getElementById('aca').textContent = result;
+let display = document.getElementById('display');
+
+function clearDisplay() {
+  display.value = '';
 }
 
-aca();
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function calculateResult() {
+  try {
+    const result = Function('"use strict";return (' + display.value + ')')();
+    display.value = result;
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
